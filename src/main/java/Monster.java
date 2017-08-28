feb2279528413ec35f5c58a027b45874065adc41
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-08-28 12:08:30 +0300

    added metods to return monster attributes and override equals

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,6 +1,30 @@
 public class Monster {
+  private String name;
 
   public Monster(String name, int personId) {
+    this.name = name;
   }
+
+//gets Monster name
+  public String getName(){
+    return name;
+  }
+
+//instantiates monster with person id
+  public int getPersonId(){
+   return personId;
+ }
+
+//overrides equals
+ @Override
+public boolean equals(Object otherMonster){
+  if (!(otherMonster instanceof Monster)) {
+    return false;
+  } else {
+    Monster newMonster = (Monster) otherMonster;
+    return this.getName().equals(newMonster.getName()) &&
+           this.getPersonId() == newMonster.getPersonId();
+  }
+}
 
 }
--- src/test/java/MonsterTest.java
+++ src/test/java/MonsterTest.java
@@ -14,4 +14,26 @@ public class MonsterTest {
     assertEquals(true, testMonster instanceof Monster);
   }
 
+//instantiates Monster with a name
+  @Test
+public void Monster_instantiatesWithName_String() {
+  Monster testMonster = new Monster("Bubbles", 1);
+  assertEquals("Bubbles", testMonster.getName());
+}
+
+//instantiates Monster with person id
+@Test
+public void Monster_instantiatesWithPersonId_int() {
+  Monster testMonster = new Monster("Bubbles", 1);
+  assertEquals(1, testMonster.getPersonId());
+}
+
+//overriding equals()
+@Test
+public void equals_returnsTrueIfNameAndPersonIdAreSame_true() {
+  Monster testMonster = new Monster("Bubbles", 1);
+  Monster anotherMonster = new Monster("Bubbles", 1);
+  assertTrue(testMonster.equals(anotherMonster));
+}
+
 }
