64a2546e175ae1f9fec0251b0ee1751cb1a9138a
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-08-28 14:29:21 +0300

    added constants to class monster

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 public class Monster {
   private String name;
-  private int id;
   private int personId;
+  private int id;
+  private int foodLevel;
+  private int sleepLevel;
+  private int playLevel;
+
+  public static final int MAX_FOOD_LEVEL = 3;
+  public static final int MAX_SLEEP_LEVEL = 8;
+  public static final int MAX_PLAY_LEVEL = 12;
+  public static final int MIN_ALL_LEVELS = 0;
 
   public Monster(String name, int personId) {
     this.name = name;
